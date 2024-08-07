01ef0b354404f1d170bf43f188e3dd0ad2d8fa50
spider_under_your_bed <71126384+SpiderUnderUrBed@users.noreply.github.com>
2024-08-07 20:17:31 +1200

    Create acer-module.nix

---
+++ acer-module.nix
@@ -0,0 +1,42 @@
+{ stdenv, lib, fetchFromGitHub, kernel, kmod }:
+stdenv.mkDerivation rec {
+  name = " acer-predator-turbo-and-rgb-keyboard-linux-module-${version}-${kernel.modDirVersion}";
+  version = "main";
+
+  src = fetchFromGitHub {
+    owner = "JafarAkhondali";
+    repo = "acer-predator-turbo-and-rgb-keyboard-linux-module";
+    rev = "${version}";
+#    sha256 = "sha256-xwwQGeswpkvEn/gGObFwt2wgPeeRIa4PhXbWue5hqGQ=";
+    sha256 = "sha256-1U/UnMI1laE9ZEcftnby39qBdj/fOwTIUG7t1wW892c=";
+  };
+
+   setSourceRoot = ''
+    export sourceRoot=$(pwd)/source
+  '';
+  nativeBuildInputs = kernel.moduleBuildDependencies;
+  makeFlags = kernel.makeFlags ++ [
+    "-C"
+    "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build"
+    "M=$(sourceRoot)"
+  ];
+  buildFlags = [ "modules" ];
+
+#  preInstall = ''
+#    mkdir -p $out/bin
+#    cp $src/facer.py $out/bin/facer
+#    chmod +x $out/bin/facer
+#  ''; 
+
+  installFlags = [ "INSTALL_MOD_PATH=${placeholder "out"}" ];
+  installTargets = [ "modules_install" ]; 
+
+
+  meta = with lib; {
+    description = "Improved Linux driver for Acer RGB Keyboards ";
+    homepage = "https://github.com/JafarAkhondali/acer-predator-turbo-and-rgb-keyboard-linux-module";
+    license = licenses.gpl3;
+    maintainers = [ ];
+    platforms = platforms.linux;
+  };
+}
